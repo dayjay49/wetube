02780a70c328b6b78f218fe243463b4ae1ac83db
Jae Hui Seo <dayjaycs49@gmail.com>
2020-05-07 19:23:39 -0400

    Incorporation of pug

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,7 @@
-export const userJoin = (req, res) => res.send("Join"); 
-export const userLogin = (req, res) => res.send("Login");
-export const userLogout = (req, res) => res.send("Logout");
+export const userJoin = (req, res) => res.render("join", { pageTitle: "Join"}); 
+export const userLogin = (req, res) => res.render("login", { pageTitle: "Log In"});
+export const userLogout = (req, res) => res.render("logout", { pageTitle: "Log Out"});
 
-export const users = (req, res) => res.send("Users");
-export const userDetail = (req, res) => res.send("User Details");
-export const editProfile = (req, res) => res.send("Edit Profile");
-export const changePassword = (req, res) => res.send("Change Password");
+export const userDetail = (req, res) => res.render("userDetail", { pageTitle: "User Details"});
+export const editProfile = (req, res) => res.render("editProfile", { pageTitle: "Edit Profile"});
+export const changePassword = (req, res) => res.render("changePassword", { pageTitle: "Change Password"});
