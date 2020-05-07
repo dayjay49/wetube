ebaf14e648646bf8e0f4b1a3ae4c63b5aedbc4d4
Jae Hui Seo <dayjaycs49@gmail.com>
2020-05-07 15:33:37 -0400

    mvc p3

--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -1,12 +1,14 @@
 import express from "express";
 import routes from "../routes";
+import { home, search } from "../controllers/videoController";
+import { userJoin, userLogin, userLogout } from "../controllers/userController";
 
 const globalRouter = express.Router();
 
-globalRouter.get(routes.home, (req, res) => res.send('Home'));
-globalRouter.get(routes.join, (req, res) => res.send('Join'));
-globalRouter.get(routes.login, (req, res) => res.send('Login'));
-globalRouter.get(routes.logout, (req, res) => res.send('Logout'));
-globalRouter.get(routes.search, (req, res) => res.send('Search'));
+globalRouter.get(routes.home, home);
+globalRouter.get(routes.search, search);
+globalRouter.get(routes.join, userJoin);
+globalRouter.get(routes.login, userLogin);
+globalRouter.get(routes.logout, userLogout);
 
 export default globalRouter;
