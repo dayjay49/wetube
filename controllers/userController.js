efa38aac320c72a02551a45df49bce32ed0fb85a
Jae Hui Seo <dayjaycs49@gmail.com>
2020-05-08 15:18:52 -0400

    More controller implementations

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,9 @@
 import routes from "../routes";
 
+// Join/Sign up
 export const getJoin = (req, res) => {
     res.render("join", { pageTitle: "Join"}); 
 }
-
 export const postJoin = (req, res) => {
     const {
         body: {name, email, password, password2}
@@ -15,12 +15,22 @@ export const postJoin = (req, res) => {
     } else {
         // To Do: Register User
         // To Do: Log user in
-        res.redirect(routes.home)
+        res.redirect(routes.home);
     }
 }
 
-export const userLogin = (req, res) => res.render("login", { pageTitle: "Log In"});
-export const userLogout = (req, res) => res.render("logout", { pageTitle: "Log Out"});
+// Log In
+export const getLogin = (req, res) => {
+    res.render("login", { pageTitle: "Log In"});
+}
+export const postLogin = (req, res) => {
+    res.redirect(routes.home);
+}
+
+export const logout = (req, res) => {
+    // To Do: Process Log Out
+    res.redirect(routes.home);
+}
 
 export const userDetail = (req, res) => res.render("userDetail", { pageTitle: "User Details"});
 export const editProfile = (req, res) => res.render("editProfile", { pageTitle: "Edit Profile"});
--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -1,16 +1,19 @@
 import express from "express";
 import routes from "../routes";
 import { home, search } from "../controllers/videoController";
-import { getJoin, postJoin, userLogin, userLogout } from "../controllers/userController";
+import { getJoin, postJoin, getLogin, postLogin, logout } from "../controllers/userController";
 
 const globalRouter = express.Router();
 
 globalRouter.get(routes.join, getJoin);
 globalRouter.post(routes.join, postJoin);
 
+globalRouter.get(routes.login, getLogin);
+globalRouter.post(routes.login, postLogin);
+globalRouter.get(routes.logout, logout);
+
 globalRouter.get(routes.home, home);
 globalRouter.get(routes.search, search);
-globalRouter.get(routes.login, userLogin);
-globalRouter.get(routes.logout, userLogout);
+
 
 export default globalRouter;
